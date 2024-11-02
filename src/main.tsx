5be1a7eaf5d42746f41cff5730b26540df2ebc9d
Pau Velasco Garrofé <pauvelascogarrofe@MacBook-Pro-de-Pau.local>
2024-11-02 18:49:42 +0100

    create memoryRouter

--- src/main.tsx
+++ src/main.tsx
@@ -7,14 +7,30 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
+import * as LayoutModule from "./pages/layout";
+import * as LoginModule from "./pages/login";
 
 const router = createMemoryRouter(
   createRoutesFromElements(
-    <>
-      <Route path='/' lazy={async () => import("./pages/dashboard")} />
-      <Route path='/login' lazy={async () => import("./pages/login")} />
-    </>
-  )
+    <Route
+      path='/'
+      id='root'
+      element={<LayoutModule.default />}
+      loader={LayoutModule.loader}
+      errorElement={<div>Error page</div>}>
+      <Route index element={<div>Dashboard page</div>} />
+      <Route
+        path='login'
+        element={<LoginModule.default />}
+        loader={LoginModule.loader}
+        errorElement={<div>Error page</div>}
+      />
+    </Route>
+  ),
+  {
+    initialEntries: ["/"],
+    initialIndex: 0,
+  }
 );
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
